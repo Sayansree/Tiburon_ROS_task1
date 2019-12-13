4a0c3583866fd23842650e190ba910c2b95539ef
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2019-12-14 01:06:18 +0530

    Add files via upload

---
+++ task3/THIRD/src/node1.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "node1");
+  ros::NodeHandle n;
+  image_transport::ImageTransport it(n);
+  image_transport::Publisher pub = it.advertise("cam1", 1);
+  ros::Rate loop_rate(5);
+
+  cv::VideoCapture cap(0);
+  cv::namedWindow("n1");
+  cv::startWindowThread();
+  if(!cap.isOpened())
+    return -1;
+
+  cv::Mat frame;
+  sensor_msgs::ImagePtr msg;
+
+  while (nh.ok()) {
+    cap >> frame;
+    if(!frame.empty()) {
+        cv::imshow("n1", cv_bridge::toCvShare(msg, "bgr8")->image);
+        msg= cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+        pub.publish(msg);
+        cv::waitKey(1);
+    }
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  cv::destroyAllWindow();
+  return 0;
+}
---
+++ task3/THIRD/src/node2.cpp
@@ -0,0 +1,42 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+ image_transport::Publisher pub;
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    int k=-1;
+    cv::Mat frame;
+  try
+  {
+    frame = cv_bridge::toCvShare(msg, "bgr8")->image
+    cv::imshow("n2",frame );
+    k=cv::waitKey(1)&&0xff;
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    return;
+  }
+    if(key==27 or key==13):
+            break
+    else if(key=='s')
+            cv::imwrite( "output.jpg", frame );
+    else if (key=='p'):
+            pub.publish(msg);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "node2");
+  ros::NodeHandle n;
+  image_transport::ImageTransport it(n);
+  image_transport::Subscriber sub = it.subscribe("cam1", 1, imageCallback);
+  pub = it.advertise("cam2", 1);
+  cv::namedWindow("n2");
+  cv::startWindowThread();
+  ros::spin();
+  cv::destroyAllWindow();
+  return 0;
+}
